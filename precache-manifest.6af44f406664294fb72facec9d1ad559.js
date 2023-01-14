88af6fb9237dbd72babfdd96238895c9493af933
Nithinkumar <nithinplavila@gmail.com>
2023-01-14 16:53:52 +0530

    Updates

---
+++ precache-manifest.6af44f406664294fb72facec9d1ad559.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b206111d987ed1d5744f",
+    "url": "/React-Projects/static/css/main.789a8d57.chunk.css"
+  },
+  {
+    "revision": "b206111d987ed1d5744f",
+    "url": "/React-Projects/static/js/main.b206111d.chunk.js"
+  },
+  {
+    "revision": "61c4c1d9f545cd57961a",
+    "url": "/React-Projects/static/js/1.61c4c1d9.chunk.js"
+  },
+  {
+    "revision": "5e0d4ac17d3be1723a0e",
+    "url": "/React-Projects/static/js/runtime~main.5e0d4ac1.js"
+  },
+  {
+    "revision": "a3b36d8c64fb989a820dbd1952a6d659",
+    "url": "/React-Projects/index.html"
+  }
+];
